6be29ec9356b2a79c0ca2849fce092a07c3b734c
ThakshilaJayathilake <thakshilanj11@gmail.com>
2024-12-20 09:27:52 +0530

    Order & inventory service communication with OpenFeign

--- order-service/order-service/src/main/java/com/tnj/microservices/order/service/OrderService.java
+++ order-service/order-service/src/main/java/com/tnj/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.tnj.microservices.order.service;
 
+import com.tnj.microservices.order.client.InventoryClient;
 import com.tnj.microservices.order.dto.OrderRequest;
 import com.tnj.microservices.order.model.Order;
 import com.tnj.microservices.order.repository.OrderRepository;
@@ -10,18 +11,31 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class OrderService {
 
+    private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+
     @Autowired
-    private OrderRepository orderRepository;
+    public OrderService(OrderRepository orderRepository, InventoryClient inventoryClient) {
+        this.orderRepository = orderRepository;
+        this.inventoryClient = inventoryClient;
+    }
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+//         1. Using Mockito; provide a mock response & use it to run our test
+//        2. Use Wiremock
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() +  " is not in stock");
+        }
+
     }
 }
--- order-service/order-service/src/test/java/com/tnj/microservices/order/OrderServiceApplicationTests.java
+++ order-service/order-service/src/test/java/com/tnj/microservices/order/OrderServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.tnj.microservices.order;
 
+import com.tnj.microservices.order.client.InventoryClient;
+import com.tnj.microservices.order.stubs.InventoryClientStub;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWiremock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -41,6 +44,9 @@ class OrderServiceApplicationTests {
 				    "quantity": 1
 				}
 				""";
+
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
+
 		var responseBodyString = RestAssured.given()
 				.contentType("application/json")
 				.body(submitOrderJson)

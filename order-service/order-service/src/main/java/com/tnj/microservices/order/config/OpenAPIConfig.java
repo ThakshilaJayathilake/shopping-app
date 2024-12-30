8a4a4271e0ea17bcdae9d33b97f68ca8c3af651b
ThakshilaJayathilake <thakshilanj11@gmail.com>
2024-12-30 12:41:28 +0530

    Circuit breaker for Inventory service

--- order-service/order-service/src/main/java/com/tnj/microservices/order/config/OpenAPIConfig.java
+++ order-service/order-service/src/main/java/com/tnj/microservices/order/config/OpenAPIConfig.java
@@ -1,4 +1,4 @@
-package com.tnj.microservices.order.cnofig;
+package com.tnj.microservices.order.config;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.info.Info;
